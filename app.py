be8e391d7d8196fe8bad69e7637ead9c8e1e7f8e
Anubhav Yadav <ay6666@srmist.edu.in>
2024-07-29 10:43:42 +0530

    Streamlit Dashboard File Added

---
+++ app.py
@@ -0,0 +1,12 @@
+# Import Relevant Libraries
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+# Streamlit page configuration
+st.set_page_config(
+    page_title="Salary Analysis",
+    page_icon="https://fontawesome.com/icons/chart-simple?f=classic&s=light",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
